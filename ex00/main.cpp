d1eebca10e18efd525c1db1d453756ceec6a3214
gypark <gghotted@gmail.com>
2021-05-28 15:20:30 +0900

    Add ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,53 @@
+#include "ScalarString.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+    return 1;
+
+  ScalarString scalarString(argv[1]);
+  try
+  {
+    char c = static_cast<char>(scalarString);
+    std::cout << "char : '" << c << "'\n";
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << "char : " << e.what() << '\n';
+  }
+
+  try
+  {
+    int i = static_cast<int>(scalarString);
+    std::cout << "int : " << i << "\n";
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << "int : " << e.what() << '\n';
+  }
+
+  std::cout << std::setprecision(1) << std::fixed;
+  try{
+    float f = static_cast<float>(scalarString);
+    std::cout << "float : " << f << "f\n";
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << "float : " << e.what() << "\n";
+  }
+
+  try{
+    double d = static_cast<double>(scalarString);
+    std::cout << "double : " << d << "\n";
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << "double : " << e.what() << "\n";
+  }
+
+  // double d = 100000000000000000000.0;
+  // std::cout << "real d: " << d << '\n';
+  // float f = 100000000000000000000.0;
+  // std::cout << "real f: " << f << '\n';
+}
